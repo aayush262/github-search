4714f59604cef856ca223455d0c6eb2553ecf0ca
Ayoush Moktan Lama <aayushlama26@gmail.com>
2022-06-10 17:15:30 +0545

    fix: rephype plugins

--- src/components/Repos/MarkDown.js
+++ src/components/Repos/MarkDown.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
 import { useSelector } from "react-redux";
 
 const MarkDown = () => {
   const { content } = useSelector((state) => state.repo);
   return (
     <div className="border-2 border-gray-200 rounded-md px-4 py-2 mt-2 bg-white">
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown rehypePlugins={[rehypeRaw]} children={content} />
     </div>
   );
 };
